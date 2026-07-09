655998dbcd46e0fba1bdb41da007831f67d4b404
RafifMuh <rafifmoh09@gmail.com>
2026-07-09 09:43:35 +0700

    Save temporary migration and model updates

--- app/Models/InternshipPeriod.php
+++ app/Models/InternshipPeriod.php
@@ -17,8 +17,8 @@ class InternshipPeriod extends Model
     ];
 
     protected $casts = [
-        'start_date' => 'date',
-        'end_date' => 'date',
+        'start_date' => 'date:Y-m-d',
+        'end_date' => 'date:Y-m-d',
     ];
 
     public function submissions()
