9643b79dba890528bd304234bea2638362e8db2d
rahmrafi <rafiansyah2604@gmail.com>
2026-07-21 14:48:57 +0700

    fix: resolve cache import error and add broadcasting files

--- app/Http/Controllers/Api/Admin/SettingsController.php
+++ app/Http/Controllers/Api/Admin/SettingsController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Setting;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Cache;
 
 class SettingsController extends Controller
 {
